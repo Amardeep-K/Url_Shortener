9fa3b1d1d075449ac5f54aa45ce4f1df740d93cf
Amardeep <akio190711@gmail.com>
2025-09-02 02:32:37 +0530

    updated

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,11 @@ import cors from "cors";
 
 import route from "./Routes/shortUrl.route.js";
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://shrinkeurl.netlify.app", // your Netlify frontend
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
